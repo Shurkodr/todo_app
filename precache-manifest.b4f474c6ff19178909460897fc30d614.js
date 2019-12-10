f075d2809b4989ee4a8897e4a2b39fd68f0089a1
Shurkodr <sashaglazunov2@gmail.com>
2019-12-10 14:01:45 +0200

    Updates

---
+++ precache-manifest.b4f474c6ff19178909460897fc30d614.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f23b9362044ab2e7da14888f036b7e2",
+    "url": "/react_todo-app/index.html"
+  },
+  {
+    "revision": "d8e2fe2d68c316bcccf3",
+    "url": "/react_todo-app/static/css/main.f942017b.chunk.css"
+  },
+  {
+    "revision": "1829b9bb8e30bd0eddcd",
+    "url": "/react_todo-app/static/js/2.da691fda.chunk.js"
+  },
+  {
+    "revision": "d8e2fe2d68c316bcccf3",
+    "url": "/react_todo-app/static/js/main.6d74e578.chunk.js"
+  },
+  {
+    "revision": "51af3b3c9c5f8ccb1c53",
+    "url": "/react_todo-app/static/js/runtime~main.df364381.js"
+  }
+]);
